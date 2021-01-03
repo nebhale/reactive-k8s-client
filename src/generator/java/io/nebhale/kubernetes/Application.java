dab150bd131aa48a92f340a4a60e93624b721d91
Ben Hale <bhale@vmware.com>
2021-01-03 07:58:35 -0800

    Copyright Updates
    This change updates the copyright statements to 2021
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- src/generator/java/io/nebhale/kubernetes/Application.java
+++ src/generator/java/io/nebhale/kubernetes/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2020-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,12 +25,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.swing.*;
 
 @Configuration
 @SpringBootApplication
